df86c7de65a06e5d84b45c6ab1ab0fc2f13e2b08
jiaoyulong <1650806563@qq.com>
2023-07-09 15:51:51 +0800

    日志父类声明

--- sylar/log.cc
+++ sylar/log.cc
@@ -6,20 +6,22 @@
 
 namespace sylar{
 
+// 日志事件
 class LogEvent {
 public:
+	typedef std::shared_ptr<LogEvent> ptr;
 	LogEvent();
 private:
-	const char* m_file = nullptr;
-	int32_t m_line = 0;
-	uint32_t m_threadId = 0;
-	uint32_t m_fiberId = 0;
-	uint64_t m_time;
-	std::string m_content;
+	const char* m_file = nullptr;   // 文件名
+	int32_t m_line = 0;				// 行号
+	uint32_t m_threadId = 0;        // 程序启动开始到现在的毫秒数
+	uint32_t m_fiberId = 0;         // 线程id
+	uint64_t m_time;                // 协程id
+	std::string m_content;			// 时间戳
 };
 
-// 日志器
-class Logger {
+// 日志级别
+class LogLevel {
 public:
 	enum Level {
 		DEBUG = 1,
@@ -27,24 +29,50 @@ public:
 		WARN = 3,
 		ERROR = 4,
 		FATAL = 5
-	};
+	}
+};
 
-	Logger();
+class LogFormatter {
+public:
+	typedef std::stared_ptr<LogFormatter> ptr;
 
+    std::string format(LogEvent::ptr event);
+private:
 
-}
+};
 
 // 日志输出地
 class LogAppender {
 public:
+	typedef std::stared_ptr<LogAppender> ptr;
 	virtual ~LogAppender() {}
+
+    void log(LogLevel::Level level, LogEvent::ptr event);
 private:
+	LogLevel::Level m_level;
 };
 
-class LogFormatter {
+// 日志器
+class Logger {
+public:
+	typedef std::stared_ptr<Logger> ptr;
+
+	Logger(const std::string& name = "root");
+	void log(LogLevel::Level level, const LogEvent& event);
+private:
+	std::string m_name;
+	LogAppender::ptr
+};
+
+// 输出到控制台的Appender
+class StdoutLogAppender: public LogAppender {
 
 };
 
-}
+// 输出到文件的Appender
+class FileLogAppender: public LogAppender {
 
-#endif  //__SYLAR_LOG_H
+};
+
+#endif	  //__SYLAR_LOG_H
+
